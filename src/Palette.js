d63c05cc4a6f61cf67594f903d1e24de1b6625e5
Andrii Lukich <andriilukich@gmail.com>
2020-04-03 12:29:52 +0300

    Created react app; Added mockup palette Colors component && Palette component;

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Palette extends Component{
+  render() {
+    return(
+      <div className="Palette">
+        <div className="Palette__colors">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Palette;
